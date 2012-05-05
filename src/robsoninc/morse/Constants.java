2b638130a98555e3f9b6590fedefa1845bf781cf
robson <rz.robsn@gmail.com>
2012-05-04 21:44:23 -0400

    added c2dm registration.

---
+++ src/robsoninc/morse/Constants.java
@@ -0,0 +1,7 @@
+package robsoninc.morse;
+
+public class Constants {
+	public final static String C2DM_EMAIL = "rz.robsn@gmail.com";
+	
+	public final static String PREF_FILE = "UserPref";
+}
--- src/robsoninc/morse/MorseActivity.java
+++ src/robsoninc/morse/MorseActivity.java
@@ -1,13 +1,41 @@
 package robsoninc.morse;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MorseActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		findViewById(R.id.button1).setOnClickListener(
+				new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						
+					}
+				});
+	}
+	
+	@Override
+	protected void onResume() {
+
+		super.onResume();		
+		
+		/* Registering the app to c2dm messaging */
+		Intent registrationIntent = new Intent("com.google.android.c2dm.intent.REGISTER");
+		registrationIntent.putExtra("app", PendingIntent.getBroadcast(this, 0, new Intent(), 0)); // boilerplate
+		registrationIntent.putExtra("sender", Constants.C2DM_EMAIL);
+		startService(registrationIntent);
+
+	}
+	
 }
