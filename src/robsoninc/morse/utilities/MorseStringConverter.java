f3793df0fd0b878eb528884a4026e59f4e25f34e
robsn <rz.robsn@gmail.com>
2012-05-27 18:37:49 -0400

    translated c# code into java.

--- src/robsoninc/morse/utilities/MorseStringConverter.java
+++ src/robsoninc/morse/utilities/MorseStringConverter.java
@@ -2,17 +2,17 @@ package robsoninc.morse.utilities;
 
 public class MorseStringConverter {
 
-	private char[] letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	private static char[] letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
 			'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 			'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6',
 			'7', '8', '9', ' ' };
-	private String[] morseCode = new String[] { ".-", "-...", "-.-.", "-..",
+	private static String[] morseCode = new String[] { ".-", "-...", "-.-.", "-..",
 			".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
 			"-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-",
 			".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--",
 			"....-", ".....", "-....", "--...", "---..", "----.", " " };
 
-	public String ConvertTextToMorse(String text) {
+	public static String ConvertTextToMorse(String text) {
 		text = text.toLowerCase();
 		String result = "";
 		int index = -1;
@@ -25,7 +25,7 @@ public class MorseStringConverter {
 		return result;
 	}
 
-	public String ConvertMorseToText(String text) {
+	public static String ConvertMorseToText(String text) {
 		text = "@" + text.replace(" ", "@@") + "@";
 		int index = -1;
 		for (char c : letters) {
