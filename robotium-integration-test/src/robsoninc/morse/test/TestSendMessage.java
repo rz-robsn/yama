c7ab5034b71605a83925d8e6b38b099717fb5bd1
Razafindramary Robson <robson.razafindramary@ericsson.com>
2012-06-16 10:47:24 -0400

    added first robotium test.

---
+++ robotium-integration-test/src/robsoninc/morse/test/TestSendMessage.java
@@ -0,0 +1,45 @@
+package robsoninc.morse.test;
+
+import junit.framework.Assert;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import robsoninc.morse.ActivitySendMessage;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.Smoke;
+
+public class TestSendMessage extends
+		ActivityInstrumentationTestCase2<ActivitySendMessage> {
+
+	private Solo solo;
+
+	private String recipient_user_id = "1111-222-333-4444";
+
+	public TestSendMessage() {
+		super("robsoninc.morse.test", ActivitySendMessage.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		Intent i = new Intent(this.getInstrumentation().getContext(), ActivitySendMessage.class);
+		i.putExtra("robsoninc.morse.recipient_user_id", this.recipient_user_id);
+		this.setActivityIntent(i);		
+		solo = new Solo(this.getInstrumentation(), this.getActivity());		
+	}
+
+	@Smoke
+	public void testTitleNameIncludesRecipientId() throws Exception
+	{
+		String title = this.getActivity().getTitle().toString();
+		Assert.assertTrue("Title does not include recipient id", title.contains(recipient_user_id));
+	}
+	
+	@Override	
+	protected void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
+
+}
