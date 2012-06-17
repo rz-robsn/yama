1fa69b597f9e94c0433b1fccf51ba8fa9fc79cff
Razafindramary Robson <robson.razafindramary@ericsson.com>
2012-06-17 00:42:52 -0400

    refactored.

--- morse-com/src/robsoninc/morse/Constants.java
+++ morse-com/src/robsoninc/morse/Constants.java
@@ -1,10 +1,12 @@
 package robsoninc.morse;
 
+import robsoninc.morse.utilities.Config;
+
 public class Constants {
 
 	/* c2dm parameters */
-	public final static String C2DM_EMAIL = "rz.robsn@gmail.com";
-	public final static String SERVER_URL = "http://morsecom.info.tm";
+	public final static String C2DM_EMAIL = Config.C2DM_EMAIL;
+	public final static String SERVER_URL = Config.SERVER_URL;
 	public final static String REGISTRATION_URL = SERVER_URL + "/c2dmregister";
 	public final static String USRLIST_URL = SERVER_URL + "/userlist";
 	public final static String SEND_MESSAGE_URL = SERVER_URL + "/sendMorseMessage";
