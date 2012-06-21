7a530dee65be6fa921724a0a01f45a1c4c2c683e
robsn <rz.robsn@gmail.com>
2012-06-21 00:57:26 -0400

    finally created the successfull test for c2dm receiver.

--- morse-com/tests/robsoninc/morse/ReceiverC2dmRegistrationTest.java
+++ morse-com/tests/robsoninc/morse/ReceiverC2dmRegistrationTest.java
@@ -1,14 +1,13 @@
 package robsoninc.morse;
 
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.*; 
+import static org.hamcrest.CoreMatchers.*;
 import static org.easymock.EasyMock.*;
-import static testingutilites.matchers.IsServerRegistrationHttpRequest.serverRegistrationHttpRequest;
-
-import org.apache.http.HttpRequest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import robsoninc.morse.utilities.ThreadServerRegistration;
 
 import android.app.Activity;
 import android.content.Context;
@@ -18,43 +17,57 @@ import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
-public class ReceiverC2dmRegistrationTest {
-		
-	private Intent i ;	
+public class ReceiverC2dmRegistrationTest
+{
+
+	private Intent i;
 	private String registrationId = "My fake Registration id";
-	
-	private ReceiverC2dmRegistration receiver ;
-	
-    @Before
-    public void setUp() throws Exception {
-		
+
+	private ReceiverC2dmRegistration receiver;
+
+	@Before
+	public void setUp() throws Exception
+	{
+
 		// Setting up intent Mock.
-    	i = createMock(Intent.class);
-    	expect(i.getAction()).andReturn("com.google.android.c2dm.intent.REGISTRATION");
-    	expect(i.getStringExtra("registration_id")).andReturn(this.registrationId);
-    	expect(i.getStringExtra("error")).andReturn(null);
-    	expect(i.getStringExtra("unregistered")).andReturn(null);
-    	replay(i);
-    	
-    	receiver = new ReceiverC2dmRegistration();
-    	Robolectric.setDefaultHttpResponse(200, "OK");
-    }
-	
+		i = createMock(Intent.class);
+		expect(i.getAction()).andReturn(
+		        "com.google.android.c2dm.intent.REGISTRATION");
+		expect(i.getStringExtra("registration_id")).andReturn(
+		        this.registrationId);
+		expect(i.getStringExtra("error")).andReturn(null);
+		expect(i.getStringExtra("unregistered")).andReturn(null);
+		replay(i);
+
+		receiver = new ReceiverC2dmRegistration();
+	}
+
 	@Test
-	public void testSaveRegistrationIdinPreferencesOnSuccessfullRegistration() throws Exception {
-				
-		Context c = new Activity();		
-		receiver.onReceive(c, i);			
+	public void testSaveRegistrationIdinPreferencesOnSuccessfullRegistration()
+	        throws Exception
+	{
+
+		receiver.onReceive(new Activity(), i);
+
+		SharedPreferences settings = Robolectric.getShadowApplication()
+		        .getSharedPreferences("UserPref", Context.MODE_PRIVATE);
+		assertThat(settings.getString("c2dm_id", ""),
+		        equalTo(this.registrationId));
+
 		verify(i);
-		
-		SharedPreferences settings = c.getSharedPreferences("UserPref" , Context.MODE_PRIVATE);
-		assertThat(settings.getString("c2dm_id", ""), equalTo(this.registrationId));
-		
-		// Make sure to run the ServerRegistration thread
-		Robolectric.idleMainLooper(10000);
-		
-		HttpRequest server_request = Robolectric.getSentHttpRequest(0);
-		assertThat(server_request, is(notNull()));
-		assertThat(server_request, is(serverRegistrationHttpRequest(registrationId)));
-    }
+	}
+
+	@Test
+	public void testSpawnsThreadRegistrationOnSuccessfullRegistration()
+	        throws Exception
+	{
+
+		int numOfServerThreads = ThreadServerRegistration.activeCount();
+		receiver.onReceive(new Activity(), i);
+		assertThat("The receiver did not spawn an server registration thread.",
+		        ThreadServerRegistration.activeCount(),
+		        is(equalTo(numOfServerThreads + 1)));
+
+		verify(i);
+	}
 }
