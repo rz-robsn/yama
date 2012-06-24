c92178f795172f631efdf317015dc73fdb29cc3f
Razafindramary Robson <robson.razafindramary@ericsson.com>
2012-06-24 18:58:00 -0400

    still working on touchy mode.

--- robotium-integration-test/src/robsoninc/morse/test/ActivitySendMessageTest.java
+++ robotium-integration-test/src/robsoninc/morse/test/ActivitySendMessageTest.java
@@ -51,18 +51,97 @@ public class ActivitySendMessageTest extends
 				R.string.sndmsg_space_button_value));
 		this.clickOnButtonTimes(R.string.sndmsg_short_button_value, 3);
 
-		// Sending the message
-		Assert.assertTrue("The Message to send should contain \"sos\".",
+		this.sendMessage("sos");
+	}
+
+	@Smoke
+	public void testSwitchToTouchyModeWhenTappingButton() throws Exception {
+		this.switchToTouchyMode();
+	}
+
+	@Smoke
+	public void testSendSOSWithTouchyMode() throws Exception {
+
+		this.switchToTouchyMode();
+		this.typeSOSOnTouchyMode();
+
+		Assert.assertTrue("The Activity did not display \"sos\".",
 				solo.searchText("sos"));
-		solo.clickOnButton(this.getActivity().getString(
-				R.string.sndmsg_send_button_value));
+		Assert.assertTrue("The Activity did not display \"sos\" in morse.",
+				solo.searchText("\\.\\.\\.\\-\\-\\-\\.\\.\\."));
+		
+		this.sendMessage("sos");
+	}
+
+	@Smoke
+	public void testSwitchToNormalMode() throws Exception {
+
+		this.switchToTouchyMode();
+		this.typeSOSOnTouchyMode();
+		solo.clickOnText("Normal Mode");
+
+		Assert.assertTrue("The Activity did not switch to normal mode",
+				solo.searchButton("(Short)|Long)|(Gap)", 3));
+	}
+
+	@Smoke
+	public void testWriteGapSymbolOnDrawingALineWithFinger() throws Exception {
+
+		this.switchToTouchyMode();
+
+		// Typing "s" character
+		solo.clickOnScreen(25, 25);
+
+		this.typeGap();
+		this.typeGap();
+
+		// Typing "t" character
+		solo.clickLongOnScreen(25, 25, 1);
+
+		Assert.assertTrue("The Activity did not display \"e t\".",
+				solo.searchText("e t"));
+		Assert.assertTrue("The Activity did not display \"e t\" in morse.",
+				solo.searchText("\\. \\-"));
+	}
+
+	private void typeSOSOnTouchyMode() throws Exception {
+
+		// Typing "s" character
+		solo.clickOnScreen(25, 25);
+		solo.clickOnScreen(50, 50);
+		solo.clickOnScreen(50, 50);
+
+		this.typeGap();
+
+		// Typing "o" character
+		solo.clickLongOnScreen(25, 25, 1);
+		solo.clickLongOnScreen(50, 50, 1);
+		solo.clickLongOnScreen(50, 50, 1);
+
+		this.typeGap();
+
+		// Typing "s" character
+		solo.clickOnScreen(25, 25);
+		solo.clickOnScreen(50, 50);
+		solo.clickOnScreen(50, 50);
+	}
+
+	private void typeGap() throws Exception {
+		// drawing a line.
+		solo.drag(5, 5, 25, 25, 1);
+	}
+
+	private void sendMessage(String message) throws Exception {
+		Assert.assertTrue(String.format(
+				"The Message to send should contain \"%s\".", message), 
+				solo.searchText(message));
+		solo.clickOnButton("Send");
 
 		Assert.assertTrue(
 				"The confirmation alert box text does not contain the confirmation text expected.",
 				solo.searchText(this.getActivity().getString(
-						R.string.sndmsg_send_dialog_body, "sos")));
-		solo.clickOnButton(this.getActivity().getString(
-				R.string.dialog_ok_button));
+						R.string.sndmsg_send_dialog_body, message)));
+		solo.clickOnButton("OK");
 
 		solo.waitForDialogToClose(10);
 
@@ -71,21 +150,18 @@ public class ActivitySendMessageTest extends
 				solo.waitForText(this.getActivity().getString(
 						R.string.sndmsg_send_success_message,
 						this.recipient_user_id)));
-		Assert.assertFalse(
-				"The message box did not empty itself",
-				solo.searchText("sos"));
+		Assert.assertFalse("The message box did not empty itself",
+				solo.searchText(message));
+
 	}
 
-	@Smoke
-	public void testSwitchToTouchyModeWhenTappingButton() throws Exception {
-		
+	private void switchToTouchyMode() throws Exception {
 		solo.clickOnButton("Touchy Mode");
-		Assert.assertFalse(
-				"The activity did not switch to touchy mode.",
+		Assert.assertFalse("The activity did not switch to touchy mode.",
 				solo.searchButton("(Short)|Long)|(Gap)"));
 	}
-	
-	private void clickOnButtonTimes(int resId, int times) {
+
+	private void clickOnButtonTimes(int resId, int times) throws Exception {
 		for (int i = 0; i < times; i++) {
 			solo.clickOnButton(this.getActivity().getString(resId));
 		}
--- robotium-integration-test/src/robsoninc/morse/test/ActivityTouchyModeTest.java
+++ robotium-integration-test/src/robsoninc/morse/test/ActivityTouchyModeTest.java
@@ -4,6 +4,7 @@ import com.jayway.android.robotium.solo.Solo;
 
 import junit.framework.Assert;
 import robsoninc.morse.ActivitySendMessage;
+import robsoninc.morse.R;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.Smoke;
@@ -39,86 +40,5 @@ public class ActivityTouchyModeTest extends
 	@Override
 	protected void tearDown() throws Exception {
 		solo.finishOpenedActivities();
-	}
-
-	@Smoke
-	public void testWriteSOSByTappingAnywhereOnScreen() throws Exception {
-		
-		solo.clickOnText("Touchy Mode");
-		
-		this.typeSOS();
-
-		Assert.assertTrue(
-				"The Activity did not display \"sos\".",
-				solo.searchText("sos"));
-		Assert.assertTrue(
-				"The Activity did not display \"sos\" in morse.",
-				solo.searchText("\\.\\.\\.\\-\\-\\-\\.\\.\\."));
-	}
-
-	@Smoke
-	public void testSwitchToNormalMode()
-			throws Exception {
-		
-		this.typeSOS();
-		solo.clickOnText("Normal Mode");
-		
-		Assert.assertTrue(
-				"The Activity did not switch to normal mode",
-				solo.searchButton("(Short)|Long)|(Gap)", 3));
-	}
-
-	@Smoke
-	public void testWriteGapSymbolOnDrawingALineWithFinger()
-			throws Exception {
-		
-		// Typing "s" character
-		solo.clickOnScreen(25, 25);
-				
-		this.typeGap();
-		this.typeGap();
-		
-		// Typing "t" character
-		solo.clickLongOnScreen(25, 25, 1);
-
-		Assert.assertTrue(
-				"The Activity did not display \"e t\".",
-				solo.searchText("e t"));
-		Assert.assertTrue(
-				"The Activity did not display \"e t\" in morse.",
-				solo.searchText("\\. \\-"));	
-	}
-		
-	private void typeSOS() {
-		
-		// Typing "s" character
-		solo.clickOnScreen(25, 25);
-		solo.clickOnScreen(50, 50);
-		solo.clickOnScreen(50, 50);
-		
-		this.typeGap();
-
-		// Typing "o" character
-		solo.clickLongOnScreen(25, 25, 1);
-		solo.clickLongOnScreen(50, 50, 1);
-		solo.clickLongOnScreen(50, 50, 1);
-		
-		this.typeGap();
-
-		// Typing "s" character
-		solo.clickOnScreen(25, 25);
-		solo.clickOnScreen(50, 50);
-		solo.clickOnScreen(50, 50);
-	}
-	
-	private void typeGap()
-	{
-		// drawing a line.
-		solo.drag(5, 5, 25, 25, 1);
-	}
-	
-	private void testSendMessage()
-	{
-		
 	}
 }
