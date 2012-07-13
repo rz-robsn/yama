58aa815277b25088bdda4f2911ae70dd462e78be
robson <robson.razafindramary@ericsson.com>
2012-07-12 21:31:10 -0400

    updated robotium test case and refactored

--- morse-com/src/robsoninc/morse/utilities/TouchyModeOnTouchListener.java
+++ morse-com/src/robsoninc/morse/utilities/TouchyModeOnTouchListener.java
@@ -1,44 +1,43 @@
 package robsoninc.morse.utilities;
 
+import android.content.Context;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.ViewConfiguration;
 
 public abstract class TouchyModeOnTouchListener implements OnTouchListener
 {
-    // Defines the minimum time the pointer has to press the screen continuously 
-    // so that the current gesture is recognised as a long tap (and not as a short tap), in ms.
-    private static final long LONG_TAP_THRESHOLD_TIME = 250;
-    
-    // Defines the minimum distance the pointer has to traverse so that 
+    // Defines the minimum distance the pointer has to traverse so that
     // the current gesture is seen as a line (and not as a tap).
-    private static final double LINE_DRAW_THRESHOLD_DISTANCE = 1;
-    
+    private static final float LINE_DRAW_THRESHOLD_DISTANCE = 1;
+
     private float downX;
     private float downY;
-    
+
     @Override
     public boolean onTouch(View v, MotionEvent event)
-    {   
-        Log.d("touchy", "event = " + event);
+    {
         switch (event.getAction())
         {
             case MotionEvent.ACTION_DOWN:
                 this.downX = event.getX();
                 this.downY = event.getY();
                 break;
-                
+
             case MotionEvent.ACTION_UP:
-                if (event.getEventTime() - event.getDownTime() < LONG_TAP_THRESHOLD_TIME)
+                if (event.getEventTime() - event.getDownTime() < ViewConfiguration.getLongPressTimeout())
                 {
                     this.onShortTouch();
                 }
-                else if (getDistance(this.downX, this.downY, event.getX(), event.getY()) < LINE_DRAW_THRESHOLD_DISTANCE)
+                else if (getDistance(this.downX, this.downY, event.getX(), event.getY()) < convertDpToPxl(
+                        LINE_DRAW_THRESHOLD_DISTANCE, v.getContext()))
                 {
                     this.onLongTouch();
                 }
-                else 
+                else
                 {
                     this.onLineTouch();
                 }
@@ -57,5 +56,11 @@ public abstract class TouchyModeOnTouchListener implements OnTouchListener
     private static double getDistance(float x1, float y1, float x2, float y2)
     {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    }
+
+    public static float convertDpToPxl(float lineDrawThresholdDistance, Context c)
+    {
+        // Density is the scale factor
+        return (lineDrawThresholdDistance * c.getResources().getDisplayMetrics().density + 0.5f);
     }
 }
--- robotium-integration-test/src/robsoninc/morse/test/ActivitySendMessageTest.java
+++ robotium-integration-test/src/robsoninc/morse/test/ActivitySendMessageTest.java
@@ -8,168 +8,171 @@ import robsoninc.morse.ActivitySendMessage;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.Smoke;
+import android.util.Log;
+import android.widget.TextView;
 import robsoninc.morse.R;
 
-public class ActivitySendMessageTest extends
-		ActivityInstrumentationTestCase2<ActivitySendMessage> {
+public class ActivitySendMessageTest extends ActivityInstrumentationTestCase2<ActivitySendMessage>
+{
 
-	private Solo solo;
+    private Solo solo;
 
-	private String recipient_user_id = "1111-222-333-4444";
+    private String recipient_user_id = "1111-222-333-4444";
+    private String controlViewText = "controls";
+    
+    
+    public ActivitySendMessageTest()
+    {
+        super("robsoninc.morse.test", ActivitySendMessage.class);
+    }
 
-	public ActivitySendMessageTest() {
-		super("robsoninc.morse.test", ActivitySendMessage.class);
-	}
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
+        Intent i = new Intent(this.getInstrumentation().getContext(), ActivitySendMessage.class);
+        i.putExtra("robsoninc.morse.recipient_user_id", this.recipient_user_id);
+        this.setActivityIntent(i);
+        solo = new Solo(this.getInstrumentation(), this.getActivity());
+    }
 
-		Intent i = new Intent(this.getInstrumentation().getContext(),
-				ActivitySendMessage.class);
-		i.putExtra("robsoninc.morse.recipient_user_id", this.recipient_user_id);
-		this.setActivityIntent(i);
-		solo = new Solo(this.getInstrumentation(), this.getActivity());
-	}
+    @Smoke
+    public void testTitleNameIncludesRecipientId() throws Exception
+    {
+        String title = this.getActivity().getTitle().toString();
+        Assert.assertTrue("Title does not include recipient id", title.contains(recipient_user_id));
+    }
 
-	@Smoke
-	public void testTitleNameIncludesRecipientId() throws Exception {
-		String title = this.getActivity().getTitle().toString();
-		Assert.assertTrue("Title does not include recipient id",
-				title.contains(recipient_user_id));
-	}
+    @Smoke
+    public void testSendSuccessfullySOS() throws Exception
+    {
 
-	@Smoke
-	public void testSendSuccessfullySOS() throws Exception {
+        // Typing "sos"
+        this.clickOnButtonTimes(R.string.sndmsg_short_button_value, 3);
+        solo.clickOnButton("Gap");
+        this.clickOnButtonTimes(R.string.sndmsg_long_button_value, 3);
+        solo.clickOnButton("Gap");
+        this.clickOnButtonTimes(R.string.sndmsg_short_button_value, 3);
 
-		// Typing "sos"
-		this.clickOnButtonTimes(R.string.sndmsg_short_button_value, 3);
-		solo.clickOnButton("Gap");
-		this.clickOnButtonTimes(R.string.sndmsg_long_button_value, 3);
-		solo.clickOnButton("Gap");
-		this.clickOnButtonTimes(R.string.sndmsg_short_button_value, 3);
+        this.sendMessage("sos");
+    }
 
-		this.sendMessage("sos");
-	}
+    @Smoke
+    public void testSwitchToTouchyModeWhenTappingButton() throws Exception
+    {
+        this.switchToTouchyMode();
+    }
 
-	@Smoke
-	public void testSwitchToTouchyModeWhenTappingButton() throws Exception {
-		this.switchToTouchyMode();
-	}
+    @Smoke
+    public void testSendSOSWithTouchyMode() throws Exception
+    {
 
-	@Smoke
-	public void testSendSOSWithTouchyMode() throws Exception {
+        this.switchToTouchyMode();
+        this.typeSOSOnTouchyMode();
 
-		this.switchToTouchyMode();
-		this.typeSOSOnTouchyMode();
+        Assert.assertTrue("The Activity did not display \"sos\".", solo.searchText("sos"));
+        Assert.assertTrue("The Activity did not display \"sos\" in morse.", solo.searchText("\\.\\.\\.\\-\\-\\-\\.\\.\\."));
 
-		Assert.assertTrue("The Activity did not display \"sos\".",
-				solo.searchText("sos"));
-		Assert.assertTrue("The Activity did not display \"sos\" in morse.",
-				solo.searchText("\\.\\.\\.\\-\\-\\-\\.\\.\\."));
-		
-		this.sendMessage("sos");
-	}
+        this.sendMessage("sos");
+    }
 
-	@Smoke
-	public void testSwitchToNormalMode() throws Exception {
+    @Smoke
+    public void testSwitchToNormalMode() throws Exception
+    {
 
-		this.switchToTouchyMode();
-		this.typeSOSOnTouchyMode();
-		solo.clickOnText("Normal Mode");
+        this.switchToTouchyMode();
+        this.typeSOSOnTouchyMode();
+        solo.clickOnText("Normal Mode");
 
-		Assert.assertTrue("The Activity did not switch to normal mode",
-				solo.searchButton("(Short)|Long)|(Gap)", 3));
-	}
+        Assert.assertTrue("The Activity did not switch to normal mode", solo.searchButton("(Short)|Long)|(Gap)", 3));
+    }
 
-	@Smoke
-	public void testWriteGapSymbolOnDrawingALineWithFinger() throws Exception {
+    @Smoke
+    public void testWriteGapSymbolOnDrawingALineWithFinger() throws Exception
+    {
 
-		this.switchToTouchyMode();
+        this.switchToTouchyMode();
 
-		// Typing "s" character
-		solo.clickOnScreen(25, 25);
+        // Typing "s" character
+        solo.clickOnScreen(25, 25);
 
-		this.typeGap();
-		this.typeGap();
+        this.typeGap();
+        this.typeGap();
 
-		// Typing "t" character
-		solo.clickLongOnScreen(25, 25, 1);
+        // Typing "t" character
+        solo.clickLongOnScreen(25, 25, 1);
 
-		Assert.assertTrue("The Activity did not display \"e t\".",
-				solo.searchText("e t"));
-		Assert.assertTrue("The Activity did not display \"e t\" in morse.",
-				solo.searchText("\\. \\-"));
-	}
+        Assert.assertTrue("The Activity did not display \"e t\".", solo.searchText("e t"));
+        Assert.assertTrue("The Activity did not display \"e t\" in morse.", solo.searchText("\\. \\-"));
+    }
 
-	private void typeSOSOnTouchyMode() throws Exception {
+    private void typeSOSOnTouchyMode() throws Exception
+    {
+        String controlViewText = "controls";
 
-		// Typing "s" character
-		solo.clickOnScreen(25, 25);
-		solo.clickOnScreen(50, 50);
-		solo.clickOnScreen(50, 50);
+        // Typing "s" character
+        solo.clickOnText(this.controlViewText);
+        solo.clickOnText(this.controlViewText);
+        solo.clickOnText(this.controlViewText);
 
-		this.typeGap();
+        this.typeGap();
 
-		// Typing "o" character
-		solo.clickLongOnScreen(25, 25, 1);
-		solo.clickLongOnScreen(50, 50, 1);
-		solo.clickLongOnScreen(50, 50, 1);
+        // Typing "o" character
+        solo.clickLongOnText(this.controlViewText);
+        solo.clickLongOnText(this.controlViewText);
+        solo.clickLongOnText(this.controlViewText);
+        
+        this.typeGap();
 
-		this.typeGap();
+        // Typing "s" character
+        solo.clickOnText(this.controlViewText);
+        solo.clickOnText(this.controlViewText);
+        solo.clickOnText(this.controlViewText);
+    }
 
-		// Typing "s" character
-		solo.clickOnScreen(25, 25);
-		solo.clickOnScreen(50, 50);
-		solo.clickOnScreen(50, 50);
-	}
+    private void typeGap() throws Exception
+    {
+        // drawing a line.
+        solo.drag(10, 100, 100, 100, 100);
+    }
 
-	private void typeGap() throws Exception {
-		// drawing a line.
-		solo.drag(5, 5, 25, 25, 1);
-	}
+    private void sendMessage(String message) throws Exception
+    {
+        Assert.assertTrue(String.format("The Message to send should contain \"%s\".", message), solo.searchText(message));
+        solo.clickOnButton("Send");
 
-	private void sendMessage(String message) throws Exception {
-		Assert.assertTrue(String.format(
-				"The Message to send should contain \"%s\".", message), 
-				solo.searchText(message));
-		solo.clickOnButton("Send");
+        Assert.assertTrue("The confirmation alert box text does not contain the confirmation text expected.",
+                solo.searchText(this.getActivity().getString(R.string.sndmsg_send_dialog_body, message)));
+        solo.clickOnButton("OK");
 
-		Assert.assertTrue(
-				"The confirmation alert box text does not contain the confirmation text expected.",
-				solo.searchText(this.getActivity().getString(
-						R.string.sndmsg_send_dialog_body, message)));
-		solo.clickOnButton("OK");
+        solo.waitForDialogToClose(10);
 
-		solo.waitForDialogToClose(10);
+        Assert.assertTrue("The activity did not show or showed the incorrect sending success confirmation dialog.",
+                solo.waitForText(this.getActivity().getString(R.string.sndmsg_send_success_message, this.recipient_user_id)));
+        Assert.assertFalse("The message box did not empty itself", solo.searchText(message));
 
-		Assert.assertTrue(
-				"The activity did not show or showed the incorrect sending success confirmation dialog.",
-				solo.waitForText(this.getActivity().getString(
-						R.string.sndmsg_send_success_message,
-						this.recipient_user_id)));
-		Assert.assertFalse("The message box did not empty itself",
-				solo.searchText(message));
+    }
 
-	}
+    private void switchToTouchyMode() throws Exception
+    {
+        solo.clickOnButton("Touchy Mode");
+        Assert.assertFalse("The activity did not switch to touchy mode.", solo.searchButton("(Short)|(Long)|(Gap)", true));
+        Assert.assertTrue("The activity did not switch to touchy mode.", solo.searchText("controls :", true));
+    }
 
-	private void switchToTouchyMode() throws Exception {
-		solo.clickOnButton("Touchy Mode");
-		Assert.assertFalse("The activity did not switch to touchy mode.",
-				solo.searchButton("(Short)|(Long)|(Gap)", true));
-		Assert.assertTrue("The activity did not switch to touchy mode.",
-				solo.searchText("controls :", true));		
-	}
+    private void clickOnButtonTimes(int resId, int times) throws Exception
+    {
+        for (int i = 0; i < times; i++)
+        {
+            solo.clickOnButton(this.getActivity().getString(resId));
+        }
+    }
 
-	private void clickOnButtonTimes(int resId, int times) throws Exception {
-		for (int i = 0; i < times; i++) {
-			solo.clickOnButton(this.getActivity().getString(resId));
-		}
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-		solo.finishOpenedActivities();
-	}
+    @Override
+    protected void tearDown() throws Exception
+    {
+        solo.finishOpenedActivities();
+    }
 
 }
