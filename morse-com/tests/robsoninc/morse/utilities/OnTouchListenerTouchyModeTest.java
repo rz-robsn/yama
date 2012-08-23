0b685a3e515050991b59dc78b2964dfc962e8675
robsn <rz.robsn@gmail.com>
2012-08-22 22:42:29 -0400

    Added more tests for OnTouchListenerTouchyMode.

--- morse-com/tests/robsoninc/morse/utilities/OnTouchListenerTouchyModeTest.java
+++ morse-com/tests/robsoninc/morse/utilities/OnTouchListenerTouchyModeTest.java
@@ -25,6 +25,8 @@ public class OnTouchListenerTouchyModeTest
     
     private long downTime;
     
+    private TextView textView;
+    
     @Before
     public void setUp() throws Exception
     {
@@ -32,6 +34,7 @@ public class OnTouchListenerTouchyModeTest
         modeListener = createMock(ModeListener.class);        
         touchListener = new OnTouchListenerTouchyMode(modeListener);
         downTime = SystemClock.uptimeMillis();
+        textView = new TextView(new Activity());
     }
     
     @Test
@@ -40,9 +43,41 @@ public class OnTouchListenerTouchyModeTest
         modeListener.onDit();        
         replay(modeListener);      
                 
-        touchListener.onTouch(new TextView(new Activity()), MotionEvent.obtain(downTime, downTime, MotionEvent.ACTION_DOWN, 0, 0, 0));
-        touchListener.onTouch(new TextView(new Activity()), MotionEvent.obtain(downTime, downTime + 90, MotionEvent.ACTION_UP, 0, 0, 0));
+        // Pressing the same spot for a short time
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime, MotionEvent.ACTION_DOWN, 0, 0, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 90, MotionEvent.ACTION_UP, 0, 0, 0));
     }
+    
+    @Test
+    public void getMorse_shouldCallDah() throws Exception 
+    {
+        modeListener.onDah();        
+        replay(modeListener);      
+                
+        // Pressing the same spot for a long time
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime, MotionEvent.ACTION_DOWN, 0, 0, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 110, MotionEvent.ACTION_MOVE, 0, 0, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 120, MotionEvent.ACTION_MOVE, 0, 0, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 110, MotionEvent.ACTION_MOVE, 0, 0, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 120, MotionEvent.ACTION_MOVE, 0, 0, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 150, MotionEvent.ACTION_UP, 0, 0, 0));
+    }
+
+    @Test
+    public void getMorse_shouldCallOnSpace() throws Exception 
+    {
+        modeListener.onSpace();        
+        replay(modeListener);      
+     
+        // Drawing a line
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime, MotionEvent.ACTION_DOWN, 0, 0, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 90, MotionEvent.ACTION_MOVE, 10, 10, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 100, MotionEvent.ACTION_MOVE, 20, 20, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 110, MotionEvent.ACTION_MOVE, 30, 30, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 120, MotionEvent.ACTION_MOVE, 40, 40, 0));
+        touchListener.onTouch(textView, MotionEvent.obtain(downTime, downTime + 150, MotionEvent.ACTION_UP, 40, 40, 0));
+    }
+
     
     @After
     public void tearDown() throws Exception 
